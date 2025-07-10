32e1ef599fcb5324a21e77b0d89b1a744bc0a06e
Enzo2198 <gianggvf8209@fullstack.edu.vn>
2025-07-10 09:19:19 +0700

    edit button

---
+++ 1.Todo_list/main.js
@@ -0,0 +1,63 @@
+const tasks = []
+
+const taskList = document.querySelector('#task-list')
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+
+taskList.onclick = function (e) {
+  const taskItem = e.target.closest('.task-item')
+  const taskIndex = +taskItem.getAttribute('task-index')
+  const task = tasks[taskIndex]
+
+  if(e.target.closest(".edit")) {
+    const newTitle = prompt('Enter the new task title', task.title)
+    task.title = newTitle
+    render()
+  } else if (e.target.closest(".done")) {
+    console.log('mark as done/undone')
+  } else if (e.target.closest(".delete")) {
+    console.log('delete')
+  }
+}
+
+todoForm.onsubmit = function (e) {
+  e.preventDefault();
+
+  const value = todoInput.value.trim()
+
+  if(!value) {
+    return alert('Please enter something')
+  }
+
+  const newTask = {
+    title: value,
+    completed: false
+  }
+
+  tasks.push(newTask)
+
+  // Re-render
+  render()
+
+  // Clear input
+  todoInput.value = ""
+}
+
+function render() {
+  const html = tasks.map((task, index) => `
+  <li class="task-item ${task.completed ? 'completed' : ''}" task-index="${index}">
+    <span class="task-title">${task.title}</span>
+    <div class="task-action">
+        <button class="task-btn edit">Edit</button>
+        <button class="task-btn done">${
+    task.completed ? 'Mark as undone' : 'Mark as done'
+  }</button>
+        <button class="task-btn delete">Delete</button>
+    </div>
+  </li>
+`).join("")
+
+  taskList.innerHTML = html
+}
+
+render()
